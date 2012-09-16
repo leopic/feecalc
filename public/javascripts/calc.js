8758eadeabf53fb41c937cbfaf58349f681172f2
LEOPiC <leopic@gmail.com>
2012-09-16 15:35:22 -0600

    Adding not paypal option.

--- public/javascripts/calc.js
+++ public/javascripts/calc.js
@@ -6,39 +6,45 @@ init();
 
 function feeCalc() {
   var el = document.getElementById('total-amount');
-
-  // option a
-  // paypal > boa > local, chunks of $400
-  var paypalFee = 5.5531697938,
-      boaPerTransFee = 5, // per $400
-      localBankFee = 1.50, // per $400
-      maximumPerWidthdraw = 400;
-
-  // option b
-  // bank to bank
-  var intlTransFee = 33,
-      bncrFee = 12,
-      bacFee = 15;
-
-  var amount = el.value,
-      paypalTakes = (paypalFee * amount)/100,
-      newAmount = amount - paypalTakes,
-      finalAmount = 0,
-      totalWithdrawals = 0;
-
-  console.log("---");
-  console.log("What you see in Paypal: $" + amount);
-
-  if(newAmount < 400) {
-    finalAmount = newAmount - (boaPerTransFee + localBankFee);
-  } else {
-    console.log("After Paypal: $" + newAmount);
-    console.info("Paypal took: $" + (amount - newAmount));
-    totalWithdrawals = Math.ceil(newAmount / maximumPerWidthdraw);
-    finalAmount = newAmount - ((boaPerTransFee + localBankFee) * totalWithdrawals);
-    console.log("Total in fees: $" + (amount - finalAmount) + ", after " + totalWithdrawals + ".");
-  }
   
-  console.log("What you end up with: $" + finalAmount);
+  if(el.value > 0) { // don't spend cycles 
 
+    var amount = el.value,
+        finalAmount = 0,
+        totalWithdrawals = 0;
+
+    // option a
+    // paypal > boa > local, chunks of $400
+    var paypalFee = 5.5531697938,
+        boaPerTransFee = 5, // per $400
+        localBankFee = 1.50, // per $400
+        maximumPerWidthdraw = 400,
+    // option b
+    // bank to bank
+        intlTransFee = 33,
+        bncrFee = 12,
+        bacFee = 15;
+
+    var paypalTakes = (paypalFee * amount)/100,
+        newAmount = amount - paypalTakes;
+
+    console.log("---");
+    console.log("What you startup with: $" + amount);
+
+    if(newAmount < maximumPerWidthdraw) { // under $400: use paypal + boa
+      finalAmount = newAmount - (boaPerTransFee + localBankFee);
+      console.log("What you end up using Paypal, single withdraw: $" + finalAmount);
+    } else {
+      console.log("After Paypal: $" + newAmount);
+      console.info("Paypal took: $" + (amount - newAmount));
+      totalWithdrawals = Math.ceil(newAmount / maximumPerWidthdraw);
+      finalAmount = newAmount - ((boaPerTransFee + localBankFee) * totalWithdrawals);
+      console.log("Total in fees: $" + (amount - finalAmount) + ", after " + totalWithdrawals + " withdrawals.");
+      console.log("What you end up using Paypal: $" + finalAmount);
+    }
+    if((amount - finalAmount) > (intlTransFee + bncrFee)){
+        console.log('mejor usar bncr');
+        console.log('What you end up using BNCR, bank to bank: $' + (amount - (intlTransFee + bncrFee)));
+    }
+  }
 }

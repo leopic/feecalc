99c55cb4a6f77992cfadbfa38e1026530af4dfa5
LEOPiC <leopic@gmail.com>
2012-09-17 11:24:18 -0600

    DoM pa luego.

--- public/javascripts/calc.js
+++ public/javascripts/calc.js
@@ -1,13 +1,12 @@
 function init() {
   window.onkeyup = feeCalc;
 }
-
 init();
 
 function feeCalc() {
   var el = document.getElementById('total-amount');
   
-  if(el.value > 0) { // don't spend cycles 
+  if(el.value > 0) { // don't spend cycles on less than $7 
 
     var amount = el.value,
         finalAmount = 0,
@@ -31,17 +30,22 @@ function feeCalc() {
     console.log("---");
     console.log("What you startup with: $" + amount);
 
+    // if the entire thing can be taken out in a single withdrawal
     if(newAmount < maximumPerWidthdraw) { // under $400: use paypal + boa
       finalAmount = newAmount - (boaPerTransFee + localBankFee);
       console.log("What you end up using Paypal, single withdraw: $" + finalAmount);
     } else {
-      console.log("After Paypal: $" + newAmount);
+      // if it can't be taken out a single withdrawal, calculate total withdrawals and fees
+      //document.getElementById("one-initial-amount");
+      //console.log("After Paypal: $" + newAmount);
       console.info("Paypal took: $" + (amount - newAmount));
       totalWithdrawals = Math.ceil(newAmount / maximumPerWidthdraw);
       finalAmount = newAmount - ((boaPerTransFee + localBankFee) * totalWithdrawals);
       console.log("Total in fees: $" + (amount - finalAmount) + ", after " + totalWithdrawals + " withdrawals.");
       console.log("What you end up using Paypal: $" + finalAmount);
     }
+
+    // checking if option one is too expensive
     if((amount - finalAmount) > (intlTransFee + bncrFee)){
         console.log('mejor usar bncr');
         console.log('What you end up using BNCR, bank to bank: $' + (amount - (intlTransFee + bncrFee)));
